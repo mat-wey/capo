ebcaa756c31cdb33414323c3a82fcd195ad5d0d8
Practicum <practicum@ya.ru>
2025-05-28 10:42:49 +0300

    Added new features in asasin

--- сурикат/сурикат/ViewController.swift
+++ сурикат/сурикат/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
-
+    @IBOutlet weak var label2: UILabel!
     @IBAction func buttonTapped(_ sender: UIButton) {
         label.text = "Button"
     }
